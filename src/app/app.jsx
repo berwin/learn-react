47e5a2b86054082061844828a3b5434d04b7c45c
liubowen <liubowen@liubowendeMacBook-Pro.local>
2015-10-25 18:28:34 +0800

    learn

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import {bindActionCreators, createStore} from 'redux';
+import {Provider, connect} from 'react-redux';
 
+// React Component
+class Counter extends React.Component {
+    render () {
+        const {value, onIncreaseClick} = this.props;
+
+        return (
+            <div>
+                <span>{value}</span>
+                <button onClick={onIncreaseClick}>click</button>
+            </div>
+        )
+    }
+}
+
+Counter.propTypes = {
+    value: PropTypes.number.isRequired,
+    onIncreaseClick: PropTypes.func.isRequired
+};
+
+// Action
+const increaseAction = {type: 'increase'};
+
+// Reducer
+function counter (state = {count: 0}, action) {
+    let count = state.count
+
+    switch (action.type) {
+        case 'increase':
+            return {count: count + 1}
+        default:
+            return state
+    }
+}
+
+// Store
+let store = createStore(counter);
+
+function mapStateToProps (state) {
+    return {value: state.count};
+}
+
+function mapDispatchToProps (dispatch) {
+    // return {onIncreaseClick: dispatch(increaseAction)}
+    return {onIncreaseClick: () => dispatch(increaseAction)}
+    // return bindActionCreators(increaseAction, dispatch);
+}
+
+let App = connect(mapStateToProps, mapDispatchToProps)(Counter);
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('example')
 );
-
