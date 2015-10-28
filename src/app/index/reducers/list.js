17c249ad4a17015050e0a967c675c086fa97e011
berwin <berwin1995@qq.com>
2015-10-28 14:51:28 +0800

    add reducers

---
+++ src/app/index/reducers/list.js
@@ -0,0 +1,57 @@
+import {ADD_ITEM, UPDATE_ITEM, DELETE_ITEM, UPDATE_ITEMS, DELETE_ITEMS} from '../constants/ActionTypes';
+
+/*
+ * 添加
+ *
+ * @param {String} 添加的文字
+ */
+let createItem = text => {
+  let time = Date.now();
+
+  return {
+    id: Math.random().toString(36).split('.').join(''),
+    addTime: time,
+    updateTime: time,
+    status: false,
+    text
+  }
+}
+
+/*
+ * 更新
+ *
+ * @param {String} 添加的文字
+ */
+let updateItem = ({id, ...other}, state) => {
+  let time = Date.now();
+
+  return state.map(item => {
+    item.id === id ?
+    Object.assign({}, state, other, {
+      updateTime: time
+    }) :
+    item
+  });
+}
+
+export default (state = [], action) => {
+  switch (action.type) {
+    case ADD_ITEM:
+      return [createItem(action.text)],
+
+    case UPDATE_ITEM:
+      return updateItem(action.data, state),
+
+    case DELETE_ITEM:
+      return state.filter(item => item.id !== action.id)
+
+    case UPDATE_ITEMS:
+      return state.map(item => Object.assign({}, item, action.data))
+
+    case DELETE_ITEMS:
+      return []
+
+    default:
+      return state
+  }
+}
