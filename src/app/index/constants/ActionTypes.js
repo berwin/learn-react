079b8c5e1757e40cf8643e447c7729190a2fc2be
berwin <berwin1995@qq.com>
2015-10-28 14:40:58 +0800

    update

--- src/app/index/constants/ActionTypes.js
+++ src/app/index/constants/ActionTypes.js
@@ -1,5 +1,5 @@
 export const ADD_ITEM = 'ADD_ITEM';
 export const UPDATE_ITEM = 'UPDATE_ITEM';
-export const UPDATE_ITEMS = 'UPDATE_ITEMS';
 export const DELETE_ITEM = 'DELETE_ITEM';
+export const UPDATE_ITEMS = 'UPDATE_ITEMS';
 export const DELETE_ITEMS = 'DELETE_ITEMS';
