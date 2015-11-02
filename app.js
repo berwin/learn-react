cb4a7d046836e34b06c4adeb3a0bf5cfcab9434f
berwin <berwin1995@qq.com>
2015-11-02 13:23:33 +0800

    add a Simple server

---
+++ app.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (req, res) {
+  if (req.url == '/favicon.ico') return res.end();
+
+  var file = '';
+
+  if (req.url === '/') {
+    file = fs.readFileSync(__dirname + '/src/index.html');
+    res.setHeader("Content-Type", "text/html");
+  } else {
+    file = fs.readFileSync(__dirname + '/src' + req.url);
+    if (~req.url.indexOf('.css')) res.setHeader("Content-Type", "text/css");
+  }
+
+  res.end(file);
+});
+
+server.listen(1996, function () {
+  console.log('\x1B[32m', 'Server started http://127.0.0.1:1995');
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var webpack = require("webpack");
 var webpackConfig = require('./webpack.config.js');
-var connect = require('gulp-connect');
 
 gulp.task('webpack', function () {
   webpack(webpackConfig, function () {
@@ -9,15 +8,8 @@ gulp.task('webpack', function () {
   });
 });
 
-gulp.task('connect', function() {
-  connect.server({
-    root: './src',
-    port: 1995
-  });
-});
-
 gulp.task('watch', function () {
   gulp.watch(['src/app/**'], ['webpack']);
 });
 
-gulp.task('default', ['connect', 'webpack', 'watch']);
+gulp.task('default', ['webpack', 'watch']);
